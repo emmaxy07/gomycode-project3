4482a79a823e739c437fc7078da4ac7a05744569
emmanuel <emmanueliyare7@gmail.com>
2021-10-19 23:31:11 +0100

    updated app/js

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Address(){
+  return(
+  <div>
+    <h1>1, Abeni Oluwaloni Close, Soluyi, Gbagada</h1>
+  </div>
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePhoto(){
+  return (
+    <div>
+      <img src="/Emmanuel.jpg" alt="" />
+    </div>
+  )
+}
+
+export default ProfilePhoto
